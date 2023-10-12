d26a10b1599b79b4e4c6e84cb3cae6d5cc562a3c
Lucas Trindade <trindadebra@gmail.com>
2023-10-11 23:43:51 -0300

    bugfix rotas privadas

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} **/
 const nextConfig = {
+  swcMinify: false, 
   images: {
     domains: ['lucastrindade.dev', 'tailwindui.com', 'equilibriodascartas.thetrinityweb.com.br', 'images.unsplash.com'],
   },
