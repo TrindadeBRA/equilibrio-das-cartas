af394016db58e86bcebc0165a85f042d62d9c3ba
Lucas Trindade <trindadebra@gmail.com>
2023-10-11 23:49:37 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} **/
 const nextConfig = {
-  swcMinify: false, 
+  swcMinify: false, //
   images: {
     domains: ['lucastrindade.dev', 'tailwindui.com', 'equilibriodascartas.thetrinityweb.com.br', 'images.unsplash.com'],
   },
