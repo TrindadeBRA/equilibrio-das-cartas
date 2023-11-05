e7d84009757df64544fea0d1a13bf860cbbe128c
Lucas Trindade <trindadebra@gmail.com>
2023-11-05 19:39:03 -0300

    update recent game

--- src/components/CardHistory/index.tsx
+++ src/components/CardHistory/index.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 const CardHistory = ({ posts }: any) => {
 
-  console.log(">>>>", posts);
-
   const { user } = useAuth();
+
+  console.log(posts);
 
   if (!posts) {
     return <p>Carregando...</p>;
@@ -16,11 +16,12 @@ const CardHistory = ({ posts }: any) => {
     <div>
       <div className="flex flex-col gap-4">
         {posts.map((post: any) => (
-          <div key={post.id} className='flex bg-gray-200 p-4 rounded-xl gap-4'>
-            <Image width={80} height={165} src={post.sorted_card_cover_url} alt={post.sorted_card_name} title={post.sorted_card_name} className="rounded border-2 border-[#da18ff]" />
+          <div key={post.id} className='flex bg-gray-100 p-4 rounded-xl gap-4 shadow-md'>
+            <Image width={80} height={165} src={post.sorted_card_cover_url} alt={post.sorted_card_name} title={post.sorted_card_name} className={`${post.card_position_string === 'Negativa' ? 'transform rotate-180' : ''} rounded border-2 border-[#da18ff] shadow-md`} />
             <div className="flex flex-col justify-center">
-              <span>Carta: {post.sorted_card_name}</span>
-              <span>Data: {post.formatted_post_date}</span>
+              <span>{post.sorted_card_name}</span>
+              <span>{post.card_theme_reading}</span>
+              <span className='text-gray-500'>{post.formatted_post_date}</span>
             </div>
           </div>
         ))}
