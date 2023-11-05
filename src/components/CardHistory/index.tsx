e183237bc91b9d13301b7b195b7f928706abb972
Lucas Trindade <trindadebra@gmail.com>
2023-11-05 18:45:09 -0300

    ultimos jogos

--- src/components/CardHistory/index.tsx
+++ src/components/CardHistory/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useAuth } from '../Auth/AuthProvider';
+import Image from 'next/image';
 
 const CardHistory = ({ posts }: any) => {
+
+  console.log(">>>>", posts);
+
   const { user } = useAuth();
 
   if (!posts) {
@@ -10,16 +14,17 @@ const CardHistory = ({ posts }: any) => {
 
   return (
     <div>
-        <div>
-          <h2>Seus ultimos 3 jogos:</h2>
-          <ul>
-            {posts.map((post: any) => (
-              <li key={post.ID}>
-                Nome: {post.post_title}, ID: {post.ID}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="flex flex-col gap-4">
+        {posts.map((post: any) => (
+          <div key={post.id} className='flex bg-gray-200 p-4 rounded-xl gap-4'>
+            <Image width={80} height={165} src={post.sorted_card_cover_url} alt={post.sorted_card_name} title={post.sorted_card_name} className="rounded border-2 border-[#da18ff]" />
+            <div className="flex flex-col justify-center">
+              <span>Carta: {post.sorted_card_name}</span>
+              <span>Data: {post.formatted_post_date}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
